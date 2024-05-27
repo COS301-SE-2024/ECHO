e042df8b8d21d6d7f5587f8f3f046ba02a13b0b4
u21434809 <u21434809@tuks.co.za>
2024-05-27 18:58:59 +0200

    🎉Add landing page route to app.routes.ts

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import  {LandingPageComponent}  from './pages/landing-page/landing-page.component';
 @Component({
   selector: 'app-root',
   standalone: true,
--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'landing', component: LandingPageComponent },
+  // other routes...
+];
