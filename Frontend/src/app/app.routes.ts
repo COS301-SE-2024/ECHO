05a12d594b69c487b8e357650a6f7f31257dab00
21797545 <u21797545@tuks.co.za>
2024-05-29 12:10:55 +0200

    🔖 Added Login and Register pages

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent} from "./pages/register/register.component";
 
 export const routes: Routes = [
   { path: 'landing', component: LandingPageComponent },
-  // other routes...
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
---
+++ Frontend/src/app/pages/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
