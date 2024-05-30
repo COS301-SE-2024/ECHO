aa4b63d799e25eb4cc1b39f5948ae8492fd29317
u21434809 <u21434809@tuks.co.za>
2024-05-30 13:33:27 +0200

    🚧 barebone html for Recommended Song component

--- Frontend/src/app/shared/song-recommendation/song-recommendation.component.ts
+++ Frontend/src/app/shared/song-recommendation/song-recommendation.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-song-recommendation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './song-recommendation.component.html',
   styleUrl: './song-recommendation.component.css'
 })
 export class SongRecommendationComponent {
+  title = 'Personalized Song Recommendations';
 
+  currentSong = {
+    title: 'Song Title',
+    imageUrl: 'https://placehold.co/50x50'
+  };
+  
+  recommendedSongs = [
+    { title: 'Recommended Song Title 1', imageUrl: 'https://placehold.co/50x50' },
+    { title: 'Recommended Song Title 2', imageUrl: 'https://placehold.co/50x50' },
+    { title: 'Recommended Song Title 3', imageUrl: 'https://placehold.co/50x50' }
+  ];
 }
