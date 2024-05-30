7d12513b909aa73c7506ec9a8a1f073513a1054f
u21434809 <u21434809@tuks.co.za>
2024-05-30 13:55:57 +0200

    📐 made new component responsive

--- Frontend/src/app/shared/song-recommendation/song-recommendation.component.ts
+++ Frontend/src/app/shared/song-recommendation/song-recommendation.component.ts
@@ -10,14 +10,35 @@ import { CommonModule } from '@angular/common';
 export class SongRecommendationComponent {
   title = 'Personalized Song Recommendations';
 
-  currentSong = {
-    title: 'Song Title',
-    imageUrl: 'https://placehold.co/50x50'
-  };
-  
-  recommendedSongs = [
-    { title: 'Recommended Song Title 1', imageUrl: 'https://placehold.co/50x50' },
-    { title: 'Recommended Song Title 2', imageUrl: 'https://placehold.co/50x50' },
-    { title: 'Recommended Song Title 3', imageUrl: 'https://placehold.co/50x50' }
-  ];
+ currentSong = {
+  title: 'Song Title',
+  imageUrl: 'https://placehold.co/50x50',
+  bpm: 120,
+  theme: 'Love',
+  mood: 'Happy'
+};
+
+recommendedSongs = [
+  { 
+    title: 'Recommended Song Title 1', 
+    imageUrl: 'https://placehold.co/50x50', 
+    bpm: 130, 
+    theme: 'Adventure', 
+    mood: 'Excited' 
+  },
+  { 
+    title: 'Recommended Song Title 2', 
+    imageUrl: 'https://placehold.co/50x50', 
+    bpm: 110, 
+    theme: 'Relaxation', 
+    mood: 'Calm' 
+  },
+  { 
+    title: 'Recommended Song Title 3', 
+    imageUrl: 'https://placehold.co/50x50', 
+    bpm: 140, 
+    theme: 'Workout', 
+    mood: 'Energetic' 
+  }
+];
 }
