e48315dcee18d42a6ab93621978b78e2d2754545
u21434809 <u21434809@tuks.co.za>
2024-05-30 13:20:58 +0200

    🎉 Add home component to app routes and song rmd component

---
+++ Frontend/src/app/shared/song-recommendation/song-recommendation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-song-recommendation',
+  standalone: true,
+  imports: [],
+  templateUrl: './song-recommendation.component.html',
+  styleUrl: './song-recommendation.component.css'
+})
+export class SongRecommendationComponent {
+
+}
