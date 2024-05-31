273474eb033ef4cde8b74ac6255f669a92f12d88
21797545 <u21797545@tuks.co.za>
2024-05-31 10:49:41 +0200

    🎉 Integrated Registration

--- Frontend/src/app/pages/register/register.component.ts
+++ Frontend/src/app/pages/register/register.component.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
 import { SpotifyLoginComponent} from "../../spotify-login/spotify-login.component";
+import { AuthService } from '../../services/auth.service';
+import {Router} from "@angular/router";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [
-    SpotifyLoginComponent
+    SpotifyLoginComponent,
+    FormsModule
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  username: string = '';
+  email: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
 
   spotify() {
     var email: any;
@@ -20,5 +29,28 @@ export class RegisterComponent {
 
     email.required = false;
     password.required = false;
+  }
+
+  register() {
+    if (this.username === '' || this.email === '' || this.password === '') {
+      alert('Please fill in all fields');
+      return;
+    }
+    this.authService.register(this.username, this.email, this.password).subscribe(
+      response => {
+        if (response.user) {
+          console.log('Account created successfully!', response);
+          this.router.navigate(['/landing']);
+        } else {
+          console.error('Account creation unsuccessful.', response);
+          alert('Account creation unsuccessful. Please try again.');
+        }
+      },
+      error => {
+        console.error('Error logging in user', error);
+        //this.errorMessage = 'An error occurred while logging in.';
+      }
+    );
+
   }
 }
