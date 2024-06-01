71ab5037174b4c2851a22369fb7a62a83c165471
Zion <u21655325@tuks.co.za>
2024-06-01 14:26:52 +0200

    made app default colours for tailwind
    more in wiki

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -3,6 +3,21 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      pink: {
+        light: '#F6668F',
+        DEFAULT: '#EE0258',
+      },
+      gray: {
+        background: '#191716',
+        component: '#252525' ,
+        DEFAULT: '#323232',
+        light: '#454549',
+        verylight: '#D9D9D9',
+      }
+    }
   },
   variants: {
     extend: {},
