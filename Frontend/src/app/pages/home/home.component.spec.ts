a0f33bd640c0c9a5f45822ca86ca25558d2a484f
u21434809 <u21434809@tuks.co.za>
2024-06-03 00:46:05 +0200

    chore: Remove duplicate HttpClientModule import in home.component.spec.ts and profile.component.spec.ts

--- Frontend/src/app/pages/home/home.component.spec.ts
+++ Frontend/src/app/pages/home/home.component.spec.ts
@@ -1,6 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
-import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
--- Frontend/src/app/pages/profile/profile.component.spec.ts
+++ Frontend/src/app/pages/profile/profile.component.spec.ts
@@ -1,6 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
-import { HttpClientModule } from '@angular/common/http';
 import { ProfileComponent } from './profile.component';
 
 describe('ProfileComponent', () => {
