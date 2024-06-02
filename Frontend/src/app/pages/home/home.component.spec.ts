6223c202dfaacaa557cfe20a6724abbbc63e65cb
Tristan Potgieter <tristanpotgieter2001@gmail.com>
2024-06-02 18:32:59 +0200

    🔖edited tests

--- Frontend/src/app/pages/home/home.component.spec.ts
+++ Frontend/src/app/pages/home/home.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
@@ -8,7 +8,8 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
+      declarations: [HomeComponent],
+      imports: [HttpClientTestingModule]
     })
     .compileComponents();
     
@@ -18,6 +19,8 @@ describe('HomeComponent', () => {
   });
 
   it('should create', () => {
+    const fixture = TestBed.createComponent(HomeComponent);
+    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
 });
