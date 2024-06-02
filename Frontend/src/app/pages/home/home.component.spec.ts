defd46d0a12c6d89c0615e663f59f2e33ab77008
Tristan Potgieter <tristanpotgieter2001@gmail.com>
2024-06-02 19:46:01 +0200

    🎉 actually works again

--- Frontend/src/app/pages/home/home.component.spec.ts
+++ Frontend/src/app/pages/home/home.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
@@ -8,8 +8,8 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [HomeComponent],
-      imports: [HttpClientTestingModule]
+      //declarations: [HomeComponent],
+      imports: [HomeComponent, HttpClientModule]
     })
     .compileComponents();
     
--- Frontend/src/app/pages/profile/profile.component.spec.ts
+++ Frontend/src/app/pages/profile/profile.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
 import { ProfileComponent } from './profile.component';
 
 describe('ProfileComponent', () => {
@@ -8,7 +8,7 @@ describe('ProfileComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProfileComponent]
+      imports: [ProfileComponent, HttpClientModule]
     })
     .compileComponents();
     
