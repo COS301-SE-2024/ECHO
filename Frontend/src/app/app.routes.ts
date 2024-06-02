704c4ce690f7e924e3f0839a641925a248357bdf
21797545 <u21797545@tuks.co.za>
2024-06-02 14:49:00 +0200

    🎉 Created Profile Page

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { LandingPageComponent } from './pages/landing-page/landing-page.componen
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent} from "./pages/register/register.component";
 import {HomeComponent} from "./pages/home/home.component";
+import {ProfileComponent} from './pages/profile/profile.component';
 
 export const routes: Routes = [
   { path: 'landing', component: LandingPageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'profile', component: ProfileComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/login', pathMatch: 'full' }
 ];
