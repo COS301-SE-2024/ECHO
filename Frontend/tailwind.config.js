0444fbcc31c70c56446fe7d872fb8e8f3d5f883c
Douglas Porter <92516119+Douglasj02@users.noreply.github.com>
2024-06-02 17:27:59 +0200

    Revert "Feat/search bar"

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -3,21 +3,6 @@ module.exports = {
   darkMode: 'class', // or 'media'
   theme: {
     extend: {},
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      pink: {
-        light: '#F6668F',
-        DEFAULT: '#EE0258',
-      },
-      gray: {
-        background: '#191716',
-        component: '#252525' ,
-        DEFAULT: '#323232',
-        light: '#454549',
-        verylight: '#D9D9D9',
-      }
-    }
   },
   variants: {
     extend: {},
