d70806006ca86f96ec1d486cfbc6abcd28c41c43
u21434809 <u21434809@tuks.co.za>
2024-06-08 16:59:42 +0200

    🚀Add responsive search bar component to home page

--- Frontend/src/app/shared/search-bar/search-bar.component.ts
+++ Frontend/src/app/shared/search-bar/search-bar.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { ScreenSizeService } from '../../services/screen-size-service.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.css'
 })
 export class SearchBarComponent {
+  screenSize?: string;
+  searchQuery:string = '';
+  constructor(private screenSizeService: ScreenSizeService) {}
+
+  ngOnInit() {
+    this.screenSizeService.screenSize$.subscribe(screenSize => {
+      this.screenSize = screenSize;
+    });
+  }
+  onSearchSubmit() {
+    console.log('Searching for:', this.searchQuery);
+  }
 
 }
