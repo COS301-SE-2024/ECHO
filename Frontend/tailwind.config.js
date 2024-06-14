35995e14840da7b0d8d4d029e0e35383c712fa95
Zion <u21655325@tuks.co.za>
2024-06-14 18:38:13 +0200

    🚀 removed light mode for bottom player

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -13,6 +13,8 @@ module.exports = {
                 'light-text': '#EE0258',
                 'desktop-bg': '#323232',
                 pink: {
+                    "white": '#E8D5DA',
+                    "verylight": '#FFABC2',
                     "light": '#F6668F',
                     DEFAULT: '#EE0258',
                     "dark": '#C40047'
@@ -22,7 +24,8 @@ module.exports = {
                     "component": '#252525',
                     "dark": '#323232',
                     "light": '#454549',
-                    "verylight": '#D9D9D9'
+                    "verylight": '#D9D9D9',
+                    "lightcomponent": '#9A9A9E',
                 }
             }
         },
