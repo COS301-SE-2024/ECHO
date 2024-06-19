c30ea95d34ffea941cf3a8b74ca1f725e5c5d0e8
Zion <u21655325@tuks.co.za>
2024-06-19 20:41:57 +0200

    🎉 created help menu modals

---
+++ Frontend/src/app/shared/about-us/about-us.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-us',
+  standalone: true,
+  imports: [],
+  templateUrl: './about-us.component.html',
+  styleUrl: './about-us.component.css'
+})
+export class AboutUsComponent {
+
+}
---
+++ Frontend/src/app/shared/privacy-policy/privacy-policy.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-privacy-policy',
+  standalone: true,
+  imports: [],
+  templateUrl: './privacy-policy.component.html',
+  styleUrl: './privacy-policy.component.css'
+})
+export class PrivacyPolicyComponent {
+
+}
