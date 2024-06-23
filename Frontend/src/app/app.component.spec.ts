bc901bfa4d1884823f7f6e388648a32baff4c725
21797545 <u21797545@tuks.co.za>
2024-06-23 20:36:10 +0200

    📐Fixed Frontend tests

--- Frontend/src/app/app.component.spec.ts
+++ Frontend/src/app/app.component.spec.ts
@@ -1,18 +1,84 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router, NavigationEnd, Event as RouterEvent } from '@angular/router';
+import { ReplaySubject, of } from 'rxjs';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from './services/auth.service';
+import { SpotifyService } from './services/spotify.service';
+
+// Mock classes
+class MockRouter {
+  private eventSubject = new ReplaySubject<RouterEvent>(1);
+  events = this.eventSubject.asObservable();
+
+  // Simulate navigation events
+  simulateNavigation(url: string) {
+    this.eventSubject.next(new NavigationEnd(1, url, url));
+  }
+}
+
+class MockAuthService {
+  sendTokensToServer() {
+    return of({});
+  }
+}
+
+class MockSpotifyService {
+  init() {
+    return Promise.resolve();
+  }
+
+  disconnectPlayer() {
+    // Mock implementation for disconnectPlayer
+  }
+
+  currentlyPlayingTrack$ = new ReplaySubject<any>(1); // Observable for currently playing track
+  playingState$ = new ReplaySubject<boolean>(1); // Observable for playing state
+}
 
 describe('AppComponent', () => {
-    let component: AppComponent;
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let mockRouter: MockRouter;
+  let mockAuthService: MockAuthService;
+  let mockSpotifyService: MockSpotifyService;
 
-    beforeEach(() => {
-        component = new AppComponent();
-    });
+  beforeEach(async () => {
+    mockRouter = new MockRouter();
+    mockAuthService = new MockAuthService();
+    mockSpotifyService = new MockSpotifyService();
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
-    });
+    await TestBed.configureTestingModule({
+      imports: [HttpClientModule, AppComponent],
+      providers: [
+        { provide: Router, useValue: mockRouter },
+        { provide: AuthService, useValue: mockAuthService },
+        { provide: SpotifyService, useValue: mockSpotifyService }
+      ]
+    }).compileComponents();
 
-    it('should have a title', () => {
-        expect(component.title).toBe('Echo');
-    });
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should show player on specific routes', () => {
+    mockRouter.simulateNavigation('/home');
+    fixture.detectChanges();
+    expect(component.showPlayer).toBe(true);
+
+    mockRouter.simulateNavigation('/profile');
+    fixture.detectChanges();
+    expect(component.showPlayer).toBe(true);
+  });
+
+  it('should not show player on other routes', () => {
+    mockRouter.simulateNavigation('/other');
+    fixture.detectChanges();
+    expect(component.showPlayer).toBe(false);
+  });
 });
--- Frontend/src/app/pages/user-library/user-library.component.spec.ts
+++ Frontend/src/app/pages/user-library/user-library.component.spec.ts
@@ -1,6 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { UserLibraryComponent } from './user-library.component';
+import { ThemeService } from '../../services/theme.service';
+import { AuthService } from '../../services/auth.service';
+import { SpotifyService } from '../../services/spotify.service';
+import { ScreenSizeService } from '../../services/screen-size-service.service';
+
+// Mock classes for services
+class MockThemeService {
+  switchTheme() {}
+  isDarkModeActive = () => true;
+}
+
+class MockAuthService {}
+
+class MockSpotifyService {}
+
+class MockScreenSizeService {
+  screenSize$ = {
+    subscribe: (callback: Function) => callback('large')
+  };
+}
 
 describe('UserLibraryComponent', () => {
   let component: UserLibraryComponent;
@@ -8,9 +29,18 @@ describe('UserLibraryComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UserLibraryComponent]
-    })
-    .compileComponents();
+      imports: [
+        RouterTestingModule,
+        UserLibraryComponent  // Importing as standalone component
+      ],
+      providers: [
+        { provide: ThemeService, useClass: MockThemeService },
+        { provide: AuthService, useClass: MockAuthService },
+        { provide: SpotifyService, useClass: MockSpotifyService },
+        { provide: ScreenSizeService, useClass: MockScreenSizeService }
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(UserLibraryComponent);
     component = fixture.componentInstance;
@@ -19,5 +49,17 @@ describe('UserLibraryComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should switch themes when method is called', () => {
+    const themeService = fixture.debugElement.injector.get(ThemeService) as MockThemeService;  // Correct way to get service instance
+    themeService.switchTheme();
+    expect(themeService.isDarkModeActive()).toBe(true);  // Assuming you want to check something like this
+  });
+
+  afterEach(() => {
+    if (fixture) {
+      fixture.destroy();
+    }
   });
 });
