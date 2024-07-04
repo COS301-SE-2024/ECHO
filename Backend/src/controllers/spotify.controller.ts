724bb47c477b46050373e1b3b25d100c0b17e038
21797545 <u21797545@tuks.co.za>
2024-07-04 15:36:07 +0200

    🚀Updated Spotify controller

--- Backend/src/controllers/spotify.controller.ts
+++ Backend/src/controllers/spotify.controller.ts
@@ -5,46 +5,51 @@ import { SpotifyService } from "../services/spotify.service";
 export class SpotifyController {
     constructor(private readonly spotifyService: SpotifyService) {}
 
-    @Get('currently-playing')
-    async getCurrentlyPlayingTrack(): Promise<any> {
-        return await this.spotifyService.getCurrentlyPlayingTrack();
+    @Post('currently-playing')
+    async getCurrentlyPlayingTrack(@Body() body: { accessToken: string; refreshToken: string }): Promise<any> {
+        const {accessToken,refreshToken} = body;
+        return await this.spotifyService.getCurrentlyPlayingTrack(accessToken,refreshToken);
     }
 
-    @Get('recently-played')
-    async getRecentlyPlayedTracks(): Promise<any> {
-        return await this.spotifyService.getRecentlyPlayedTracks();
+    @Post('recently-played')
+    async getRecentlyPlayedTracks(@Body() body: { accessToken: string; refreshToken: string }): Promise<any> {
+        const {accessToken,refreshToken} = body;
+        return await this.spotifyService.getRecentlyPlayedTracks(accessToken,refreshToken);
     }
 
     @Post('queue')
-    async getQueue(@Body() body: { artist: string; song_name: string }): Promise<any> {
-        const { artist, song_name } = body;
-        return await this.spotifyService.getQueue(artist, song_name);
+    async getQueue(@Body() body: { artist: string; song_name: string, accessToken: string; refreshToken: string }): Promise<any> {
+        const { artist, song_name , accessToken,refreshToken} = body;
+        return await this.spotifyService.getQueue(artist, song_name, accessToken,refreshToken);
     }
 
     @Put('play')
-    async playTrackById(@Body() body: { trackId: string, deviceId: string }): Promise<any> {
-        const { trackId, deviceId } = body;
-        return await this.spotifyService.playTrackById(trackId, deviceId);
+    async playTrackById(@Body() body: { trackId: string, deviceId: string,accessToken: string; refreshToken: string }): Promise<any> {
+        const { trackId, deviceId,accessToken,refreshToken } = body;
+        return await this.spotifyService.playTrackById(trackId, deviceId,accessToken,refreshToken);
     }
 
     @Put('pause')
-    async pause(): Promise<any> {
-        return await this.spotifyService.pause();
+    async pause(@Body() body: { accessToken: string; refreshToken: string }): Promise<any> {
+        const {accessToken,refreshToken} = body;
+        return await this.spotifyService.pause(accessToken,refreshToken);
     }
 
     @Put('resume')
-    async play(): Promise<any> {
-        return await this.spotifyService.play();
+    async play(@Body() body: { accessToken: string; refreshToken: string }): Promise<any> {
+        const {accessToken,refreshToken} = body;
+        return await this.spotifyService.play(accessToken,refreshToken);
     }
 
     @Put('volume')
-    async setVolume(@Body() body: { volume: number }): Promise<any> {
-        const { volume } = body;
-        return await this.spotifyService.setVolume(volume);
+    async setVolume(@Body() body: { volume: number, accessToken: string; refreshToken: string }): Promise<any> {
+        const {volume,accessToken,refreshToken} = body;
+        return await this.spotifyService.setVolume(volume,accessToken,refreshToken);
     }
 
     @Post('track-details')
-    async getTrackDetails(@Body() body: { trackID: string }): Promise<any> {
-        return await this.spotifyService.getTrackDetails(body.trackID);
+    async getTrackDetails(@Body() body: { trackID: string, accessToken: string; refreshToken: string }): Promise<any> {
+        const {trackID,accessToken,refreshToken} = body;
+        return await this.spotifyService.getTrackDetails(trackID,accessToken,refreshToken);
     }
 }
