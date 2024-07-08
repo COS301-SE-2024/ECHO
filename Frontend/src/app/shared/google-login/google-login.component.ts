17862c5a0c053663a9d138fd269cf39e60072092
21797545 <u21797545@tuks.co.za>
2024-07-08 13:51:07 +0200

    🎉Google login button

---
+++ Frontend/src/app/shared/google-login/google-login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-google-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './google-login.component.html',
+  styleUrl: './google-login.component.css'
+})
+export class GoogleLoginComponent {
+
+  loginWithGoogle() {
+
+  }
+}
