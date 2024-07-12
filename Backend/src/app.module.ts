334e401ba187d2d361469bb2d2e3937195b7ea43
21797545 <u21797545@tuks.co.za>
2024-07-12 14:38:56 +0200

    📐Added youtube controller & service to app module

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { HttpModule } from '@nestjs/axios'; // Import HttpModule
+import { HttpModule } from '@nestjs/axios';
 import { AuthController } from './auth/controller/auth.controller';
 import { AuthService } from './auth/services/auth.service';
 import { SupabaseService } from './supabase/services/supabase.service';
 import { TokenMiddleware } from './middleware/token.middleware';
 import { SpotifyController } from './spotify/controller/spotify.controller';
 import { SpotifyService } from './spotify/services/spotify.service';
+import { YoutubeController } from "./youtube/controller/youtube.controller";
+import { YoutubeService } from "./youtube/services/youtube.service";
 
 @Module({
     imports: [
@@ -15,8 +17,8 @@ import { SpotifyService } from './spotify/services/spotify.service';
         }),
         HttpModule, // Include HttpModule here
     ],
-    controllers: [AuthController, SpotifyController],
-    providers: [AuthService, SupabaseService, ConfigService, SpotifyService],
+    controllers: [AuthController, SpotifyController, YoutubeController],
+    providers: [AuthService, SupabaseService, ConfigService, SpotifyService, YoutubeService],
 })
 export class AppModule {
     configure(consumer: MiddlewareConsumer) {
