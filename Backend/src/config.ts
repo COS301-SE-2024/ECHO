347c544f8a120f7756cae8c2eae6fb67c0a92f81
21797545 <u21797545@tuks.co.za>
2024-07-12 14:38:33 +0200

    📐Updated config to include youtube API key

--- Backend/src/config.ts
+++ Backend/src/config.ts
@@ -8,3 +8,5 @@ export const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 export const encryptionKey = process.env.SECRET_ENCRYPTION_KEY;
 
 export const accessKey = process.env.ACCESS_KEY;
+
+export const youtubeKey = process.env.YOUTUBE_KEY;
