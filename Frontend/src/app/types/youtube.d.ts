acde3fb195b5d8aac928cb9677d9cf57949251bd
21797545 <u21797545@tuks.co.za>
2024-07-15 11:24:55 +0200

    🎉Custom YouTube type

---
+++ Frontend/src/app/types/youtube.d.ts
@@ -0,0 +1,38 @@
+declare namespace YT {
+  interface Player {
+    loadVideoById(videoId: string): void;
+    getCurrentTime(): number;
+    getDuration(): number;
+    pauseVideo(): void;
+    playVideo(): void;
+    setVolume(volume: number): void;
+    getVideoData(): any;
+    destroy(): void;
+  }
+
+  interface PlayerEvent {
+    target: Player;
+  }
+
+  interface OnStateChangeEvent {
+    data: number;
+  }
+
+  enum PlayerState {
+    PLAYING = 1,
+    PAUSED = 2,
+    ENDED = 0
+  }
+
+  interface PlayerOptions {
+    height?: string | number;
+    width?: string | number;
+    videoId?: string;
+    events?: {
+      onReady?: (event: PlayerEvent) => void;
+      onStateChange?: (event: OnStateChangeEvent) => void;
+    };
+  }
+
+  function Player(elementId: string, options: PlayerOptions): Player;
+}
