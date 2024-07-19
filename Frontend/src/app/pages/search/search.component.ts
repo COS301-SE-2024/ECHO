a41e36b80804e0fae955147d144cf6ee35db6553
21797545 <u21797545@tuks.co.za>
2024-07-19 11:19:42 +0200

    🚀Search integrated

--- Frontend/src/app/pages/search/search.component.ts
+++ Frontend/src/app/pages/search/search.component.ts
@@ -1,32 +1,27 @@
-import { Component,Input } from '@angular/core';
-import { NgClass, NgForOf, NgIf } from '@angular/common';
+import { Component, Input, OnInit } from "@angular/core";
+import { NgClass, NgForOf, NgIf, AsyncPipe } from '@angular/common';
+import { SearchService, Track } from "../../services/search.service";
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [NgIf,NgForOf,NgClass],
+  imports: [NgIf, NgForOf, NgClass, AsyncPipe],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
   @Input() searchQuery!: string;
+  songs$: Observable<Track[]>;
+  albums$: Observable<Track[]>;
+  topResult$: Observable<Track>;
 
-  
-  songs : any = [ 
-    {},
-    {},
-    {} ,{},
-    {},
-    {} ,{},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {}
-];
+  constructor(private searchService: SearchService) {
+    this.songs$ = this.searchService.getSearch();
+    this.albums$ = this.searchService.getAlbumSearch();
+    this.topResult$ = this.searchService.getTopResult();
+  }
+
+  ngOnInit() {
+  }
 }
