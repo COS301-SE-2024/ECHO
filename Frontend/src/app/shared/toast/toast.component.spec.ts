0e61237f9600e1c19310337217fcb1503c03491d
Tristan Potgieter <tristanpotgieter2001@gmail.com>
2024-07-23 16:38:20 +0200

    🚀Started improving coverage for frontend

--- Frontend/src/app/shared/toast/toast.component.spec.ts
+++ Frontend/src/app/shared/toast/toast.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { ToastComponent } from './toast.component';
 import { CommonModule } from '@angular/common';
+import { IterableDiffers } from '@angular/core';
 
 describe('ToastComponent', () => {
   let component: ToastComponent;
@@ -48,12 +49,41 @@ describe('ToastComponent', () => {
     expect(component.close.emit).toHaveBeenCalled();
     }));
 
-it('hideToast should set isVisible to false and emit close', () => {
-  jest.spyOn(component.close, 'emit');
-  component.isVisible = true; // Manually set to simulate visible toast
-  component.hideToast();
-  expect(component.isVisible).toBe(false);
-  expect(component.close.emit).toHaveBeenCalled();
-});
+  it('hideToast should set isVisible to false and emit close', () => {
+    jest.spyOn(component.close, 'emit');
+    component.isVisible = true; // Manually set to simulate visible toast
+    component.hideToast();
+    expect(component.isVisible).toBe(false);
+    expect(component.close.emit).toHaveBeenCalled();
+  });
+
+  describe('getToastClasses', () => {
+    it('should return correct class for success type', () => {
+      component.type = 'success';
+      expect(component.getToastClasses()).toEqual({
+        'bg-green-500': true,
+        'bg-red-500': false,
+        'bg-blue-500': false,
+      });
+    });
+  
+    it('should return correct class for error type', () => {
+      component.type = 'error';
+      expect(component.getToastClasses()).toEqual({
+        'bg-green-500': false,
+        'bg-red-500': true,
+        'bg-blue-500': false,
+      });
+    });
+  
+    it('should return correct class for info type', () => {
+      component.type = 'info';
+      expect(component.getToastClasses()).toEqual({
+        'bg-green-500': false,
+        'bg-red-500': false,
+        'bg-blue-500': true,
+      });
+    });
+  });
 
 });
