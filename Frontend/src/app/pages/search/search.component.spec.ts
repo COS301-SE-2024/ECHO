64f261da9df1f1994caae99872b8323e2759e71a
Tristan Potgieter <tristanpotgieter2001@gmail.com>
2024-07-28 16:22:02 +0200

    🔰 search.component.ts covered

--- Frontend/src/app/pages/search/search.component.spec.ts
+++ Frontend/src/app/pages/search/search.component.spec.ts
@@ -1,14 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchComponent } from './search.component';
+import { SearchService, Track } from '../../services/search.service';
+import { of } from 'rxjs';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
+  let searchServiceMock: any;
 
   beforeEach(async () => {
+    searchServiceMock = {
+      getSearch: jest.fn().mockReturnValue(of([])),
+      getAlbumSearch: jest.fn().mockReturnValue(of([])),
+      getTopResult: jest.fn().mockReturnValue(of({} as Track)),
+    };
     await TestBed.configureTestingModule({
-      imports: [SearchComponent]
+      providers: [
+        SearchComponent,
+        { provide: SearchService, useValue: searchServiceMock }
+      ]
     })
     .compileComponents();
 
@@ -19,5 +30,14 @@ describe('SearchComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call searchService methods and set observables', () => {
+    expect(searchServiceMock.getSearch).toHaveBeenCalled();
+    expect(searchServiceMock.getAlbumSearch).toHaveBeenCalled();
+    expect(searchServiceMock.getTopResult).toHaveBeenCalled();
+    expect(component.songs$).toBeTruthy();
+    expect(component.albums$).toBeTruthy();
+    expect(component.topResult$).toBeTruthy();
   });
 });
