daf0fe3e5b63428b47715f259ccee3be22919295
Tristan Potgieter <tristanpotgieter2001@gmail.com>
2024-07-28 15:32:48 +0200

    🔰 search-bar.component.ts statement coverage 88.88%

--- Frontend/src/app/shared/search-bar/search-bar.component.spec.ts
+++ Frontend/src/app/shared/search-bar/search-bar.component.spec.ts
@@ -1,14 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchBarComponent } from './search-bar.component';
+import { SearchService } from '../../services/search.service';
+import { ScreenSizeService } from '../../services/screen-size-service.service';
+import { of } from 'rxjs';
 
 describe('SearchBarComponent', () => {
   let component: SearchBarComponent;
   let fixture: ComponentFixture<SearchBarComponent>;
+  let screenSizeServiceMock: any;
+  let searchServiceMock: any;
 
   beforeEach(async () => {
+
+    screenSizeServiceMock = {
+      screenSize$: of('large')
+    };
+
+    searchServiceMock = {
+      storeSearch: jest.fn().mockReturnValue(of({})),
+      storeAlbumSearch: jest.fn().mockReturnValue(of({}))
+    };
+
     await TestBed.configureTestingModule({
-      imports: [SearchBarComponent]
+      imports: [SearchBarComponent],
+      providers: [
+        { provide: ScreenSizeService, useValue: screenSizeServiceMock },
+        { provide: SearchService, useValue: searchServiceMock }
+      ]
     })
     .compileComponents();
 
@@ -19,5 +38,19 @@ describe('SearchBarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('onSearchSubmit', () => {
+    it('should emit searchDown and call searchService on search submit', () => {
+      jest.spyOn(component.searchDown, 'emit');
+      const searchQuery = 'test query';
+      component.searchQuery = searchQuery;
+  
+      component.onSearchSubmit();
+  
+      expect(component.searchDown.emit).toHaveBeenCalledWith(searchQuery);
+      expect(searchServiceMock.storeSearch).toHaveBeenCalledWith(searchQuery);
+      expect(searchServiceMock.storeAlbumSearch).toHaveBeenCalledWith(searchQuery);
+    });
   });
 });
