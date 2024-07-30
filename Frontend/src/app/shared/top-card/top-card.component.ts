d3d1c8b64067dd4b5e71fd168994bb37144e077f
Rueben  van der Westhuizen <91849806+21434809@users.noreply.github.com>
2024-07-30 20:34:45 +0200

    📐wrong import

--- Frontend/src/app/shared/top-card/top-card.component.ts
+++ Frontend/src/app/shared/top-card/top-card.component.ts
@@ -1,10 +1,10 @@
 import { Component,Input } from '@angular/core';
 import { MoodService } from '../../services/mood-service.service';
-import { NgModule } from '@angular/core';
+import { NgClass} from '@angular/common';
 @Component({
   selector: 'app-top-card',
   standalone: true,
-  imports: [NgModule],
+  imports: [NgClass],
   templateUrl: './top-card.component.html',
   styleUrl: './top-card.component.css'
 })
@@ -15,8 +15,10 @@ export class TopCardComponent {
   //Mood Service Variables
   moodComponentClasses!:{ [key: string]: string };
   backgroundMoodClasses!:{ [key: string]: string };
+  MoodClassesDark!:{[key: string]: string};
   constructor(public moodService: MoodService) {
     this.moodComponentClasses = this.moodService.getComponentMoodClasses(); 
     this.backgroundMoodClasses = this.moodService.getBackgroundMoodClasses();
+    this.MoodClassesDark = this.moodService.getComponentMoodClassesDark();
   }
 }
