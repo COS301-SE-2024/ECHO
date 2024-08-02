c6b251864eff5974ce966090b762d804d6bbd2d6
Zion <u21655325@tuks.co.za>
2024-08-02 20:35:31 +0200

    🚧 added header to app

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -10,10 +10,11 @@ import { filter } from 'rxjs/operators';
 import { NgIf } from "@angular/common";
 import { SideBarComponent } from "./shared/side-bar/side-bar.component";
 import { ProviderService } from "./services/provider.service";
+import { PageHeaderComponent } from "./shared/page-header/page-header.component";
 @Component({
     selector: 'app-root',
     standalone: true,
-  imports: [RouterOutlet, BottomPlayerComponent, NgIf, SideBarComponent, BottomNavComponent],
+  imports: [RouterOutlet, BottomPlayerComponent, NgIf, SideBarComponent, BottomNavComponent, PageHeaderComponent],
 
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
@@ -24,6 +25,8 @@ export class AppComponent {
   screenSize?: string;
   showPlayer = false;
   displayPlayer = false;
+  currentPage: string = '';
+  displayPageName: boolean = false;
 
   constructor(private router: Router, private screenSizeService: ScreenSizeService, private providerService: ProviderService, updates: SwUpdate) {
     this.router.events.pipe(
@@ -31,6 +34,27 @@ export class AppComponent {
     ).subscribe((event: RouterEvent) => {
       if (event instanceof NavigationEnd) {
         this.showPlayer = ['/home', '/profile'].includes(event.urlAfterRedirects);
+        switch (event.urlAfterRedirects) {
+          case '/home':
+            this.currentPage = 'Home';
+            this.displayPageName = true;
+            break;
+          case '/profile':
+            this.currentPage = 'Profile';
+            this.displayPageName = true;
+            break;
+          case '/settings':
+            this.currentPage = 'Settings';
+            this.displayPageName = true;
+            break;
+          case '/search':
+            this.currentPage = 'Search';
+            this.displayPageName = true;
+            break;
+          default:
+            this.currentPage = '';
+            this.displayPageName = false;
+        }
       }
     });
     this.router.events.pipe(
@@ -58,3 +82,4 @@ export class AppComponent {
     });
   }
 }
+
