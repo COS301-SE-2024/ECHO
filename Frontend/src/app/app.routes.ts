b47f88f385646b50932a1cbe29c9624eb737338e
Zion <u21655325@tuks.co.za>
2024-08-06 22:19:31 +0200

    🆕 created fragmented routes for navbar

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
 import { LandingPageComponent } from "./pages/landing-page/landing-page.component";
 import { LoginComponent } from "./pages/login/login.component";
 import { RegisterComponent } from "./pages/register/register.component";
@@ -7,14 +7,16 @@ import { ProfileComponent } from "./pages/profile/profile.component";
 import { AuthCallbackComponent } from "./authcallback/authcallback.component";
 import { UserLibraryComponent } from "./pages/user-library/user-library.component";
 import { ArtistProfileComponent } from "./pages/artist-profile/artist-profile.component";
-import {SearchComponent} from "./pages/search/search.component";
+import { SearchComponent } from "./pages/search/search.component";
 import { SettingsComponent } from "./pages/settings/settings.component";
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
   { path: "landing", component: LandingPageComponent },
   { path: "login", component: LoginComponent },
   { path: "register", component: RegisterComponent },
   { path: "home", component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: "profile", component: ProfileComponent },
   { path: "auth/callback", component: AuthCallbackComponent },
   { path: "", redirectTo: "/login", pathMatch: "full" },
@@ -24,3 +26,10 @@ export const routes: Routes = [
   { path: "artist-profile", component: ArtistProfileComponent},
   //{ path: "**", redirectTo: "/login", pathMatch: "full" },
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule]
+})
+
+export class AppRoutesModule { }
