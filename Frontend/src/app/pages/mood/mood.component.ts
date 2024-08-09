409ada2bd3ac0770dfb0bdda44d0106119702a0d
Zion <u21655325@tuks.co.za>
2024-08-09 14:30:17 +0200

    🚧 page layout edits

--- Frontend/src/app/pages/mood/mood.component.ts
+++ Frontend/src/app/pages/mood/mood.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
-import { NgForOf, NgIf, NgClass } from '@angular/common';
+import { NgForOf, NgIf, NgClass, NgSwitch, NgSwitchCase } from '@angular/common';
 import { ScreenSizeService } from '../../services/screen-size-service.service';
 import { MoodService } from "../../services/mood-service.service";
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { Router } from '@angular/router';
+import { SearchBarComponent } from '../../shared/search-bar/search-bar.component';
+import { ProfileComponent } from '../profile/profile.component';
+import { MoodDropDownComponent } from '../../shared/mood-drop-down/mood-drop-down.component';
 
 
 @Component({
   selector: 'app-mood',
   standalone: true,
-  imports: [ NgForOf, NgIf, NgClass, NavbarComponent ],
+  imports: [ NgForOf, NgIf, NgClass, NgSwitch, NgSwitchCase, NavbarComponent, SearchBarComponent, ProfileComponent, MoodDropDownComponent ],
   templateUrl: './mood.component.html',
   styleUrl: './mood.component.css'
 })
@@ -16,10 +20,14 @@ export class MoodComponent {
     screenSize?: string;
     moodComponentClasses!:{ [key: string]: string };
     backgroundMoodClasses!:{ [key: string]: string };
+    currentSelection: string = 'All';
+    searchQuery: string = '';
+    title: string = 'Mood';
     
     constructor(
       private screenSizeService: ScreenSizeService,
       public moodService: MoodService,
+      private router: Router,
     ){
         this.moodComponentClasses = this.moodService.getComponentMoodClasses(); 
         this.backgroundMoodClasses = this.moodService.getBackgroundMoodClasses();
@@ -32,4 +40,14 @@ export class MoodComponent {
     }
 
     onNavChange($event: string) {}
+
+    onSearchdown(subject:string) {
+      this.searchQuery = subject;
+      this.title = 'Search';
+      this.router.navigate(['/home'], { fragment: 'search' });
+  }
+
+  profile() {
+    this.router.navigate(['/profile']);
+}
 }
