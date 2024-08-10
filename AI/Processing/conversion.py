706bbba72d5e2b2bdbb50cf17e999f28f639b43e
MariePretorius <u21565342@tuks.co.za>
2024-08-10 12:49:04 +0200

    📐 changed what data is stored in db

--- AI/Processing/conversion.py
+++ AI/Processing/conversion.py
@@ -1,9 +1,6 @@
 import csv
 import json
 
-input_csv_file = 'expanded_details.csv'
-output_json_file = 'songs.json'
-
 
 def csv_to_json(input_csv_file, output_json_file):
     with open(input_csv_file, mode='r', encoding='utf-8') as csv_file:
@@ -37,4 +34,29 @@ def csv_to_json(input_csv_file, output_json_file):
         json.dump(songs, json_file, indent=4)
 
 
-csv_to_json(input_csv_file, output_json_file)
+def update_csv():
+    with open('songs.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    filtered_data = []
+
+    for song in data:
+        filtered_song = {
+            "SongName": song.get("Song Name", ""),
+            "Artist": song.get("Artist", ""),
+            "URI": song.get("Song Features", {}).get("uri", ""),
+            "ClusterNumber": song.get("Cluster Number", ""),
+            "Emotion": song.get("Emotion", ""),
+            "AlbumGenre": song.get("Album Genre", "")
+        }
+        filtered_data.append(filtered_song)
+
+    output_file = "filtered_songs.json"
+
+    with open(output_file, 'w', encoding='utf-8') as file:
+        json.dump(filtered_data, file, ensure_ascii=False, indent=4)
+
+    print(f"Filtered data written to {output_file}")
+
+
+update_csv()
