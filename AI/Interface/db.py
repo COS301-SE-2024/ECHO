687e6a95c0278ebdecc5aa2d09cafe6b4c7f88c5
MariePretorius <u21565342@tuks.co.za>
2024-08-11 11:57:12 +0200

    🌍 store recommendations for faster access

--- AI/Interface/db.py
+++ AI/Interface/db.py
@@ -1,23 +1,15 @@
-import json
 import os
-import sys
-import uuid
 
-import dotenv
-
-from azure.core.exceptions import AzureError
 from azure.cosmos import CosmosClient, PartitionKey
 import azure.cosmos.exceptions as exceptions
-from azure.cosmos import ThroughputProperties
 
-dotenv.load_dotenv()
-
-CONNECTION_STRING = os.getenv("PRIMARY_CONNECTION_STRING")
+CONNECTION_STRING = os.environ.get("PRIMARY_CONNECTION_STRING")
 ENDPOINT = os.environ.get("COSMOS_ENDPOINT")
 KEY = os.environ.get("COSMOS_KEY")
 
 DATABASE_ID = "echo-music"
 CONTAINER_ID = "music-store"
+RECOMMENDATIONS_ID = "music-recommendations"
 
 client = CosmosClient.from_connection_string(conn_str=CONNECTION_STRING)
 
@@ -55,6 +47,16 @@ def get_container(database):
         print('A container with id \'{0}\' does not exist'.format(id))
 
 
+def get_recommendations_container(database):
+    try:
+        container = database.get_container_client(RECOMMENDATIONS_ID)
+        container.read()
+        return container
+
+    except exceptions.CosmosResourceNotFoundError:
+        print('A container with id \'{0}\' does not exist'.format(id))
+
+
 def create_container(database):
     try:
         partition_key_path = PartitionKey(path="/TrackID")
@@ -75,11 +77,20 @@ def store_song(song):
     
     existing_song = check_id(song['URI'])
     if existing_song:
-        print("already in database")
         return
     else:
         container.create_item(body=song)
-        print("song added")
+
+
+def store_recommendations(songs):
+    database = get_database()
+    container = get_recommendations_container(database)
+
+    existing_song = check_recommendations(songs['URI'])
+    if existing_song:
+        return
+    else:
+        container.create_item(body=songs)
 
 
 def read_all(container):
@@ -95,25 +106,24 @@ def read_from_cluster(cluster_number):
     item_list = list(container.query_items(query=query, enable_cross_partition_query=True))
 
     return item_list
-
-
-# def in_database(uri):
-#     database = get_database()
-#     container = get_container(database)
-
-#     query = f"SELECT * FROM c WHERE c[URI] = '{uri}'"
-#     item_list = list(container.query_items(query=query, enable_cross_partition_query=True))
-
-#     if item_list:
-#         print(item_list[0])
-#         return item_list[0]
-#     else:
-#         return None
     
 
 def check_id(song_id):
     database = get_database()
     container = get_container(database)
+
+    query = f"SELECT * FROM c WHERE c['URI'] = '{song_id}'"
+    item_list = list(container.query_items(query=query, enable_cross_partition_query=True))
+
+    if item_list:
+        return item_list[0]
+    else:
+        return None
+    
+
+def check_recommendations(song_id):
+    database = get_database()
+    container = get_recommendations_container(database)
 
     query = f"SELECT * FROM c WHERE c['URI'] = '{song_id}'"
     item_list = list(container.query_items(query=query, enable_cross_partition_query=True))
@@ -138,4 +148,4 @@ def check_id(song_id):
 
 # store_song(song)
 
-check_id("spotify:track:5kr3j5Clb9rjEposoMyLVt")
+# check_id("spotify:track:5kr3j5Clb9rjEposoMyLVt")
