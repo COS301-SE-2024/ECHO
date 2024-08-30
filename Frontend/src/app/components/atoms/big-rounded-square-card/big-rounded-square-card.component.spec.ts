67bb47cc8527274025e52dcf9f54c07b0304b2f1
Rueben  van der Westhuizen <91849806+21434809@users.noreply.github.com>
2024-08-30 11:27:02 +0200

    🚀 Add BigRoundedSquareCardComponent and update MoodsListComponent to use

---
+++ Frontend/src/app/components/atoms/big-rounded-square-card/big-rounded-square-card.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BigRoundedSquareCardComponent } from './big-rounded-square-card.component';
+
+describe('BigRoundedSquareCardComponent', () => {
+  let component: BigRoundedSquareCardComponent;
+  let fixture: ComponentFixture<BigRoundedSquareCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [BigRoundedSquareCardComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BigRoundedSquareCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- Frontend/src/app/components/molecule/moods-list/moods-list.component.ts
+++ Frontend/src/app/components/molecule/moods-list/moods-list.component.ts
@@ -1,10 +1,11 @@
 // moods-list.component.ts
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BigRoundedSquareCardComponent } from '../../atoms/big-rounded-square-card/big-rounded-square-card.component';
 @Component({
   selector: 'app-moods-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,BigRoundedSquareCardComponent],
   templateUrl: './moods-list.component.html',
   styleUrls: ['./moods-list.component.css']
 })
