7493aeec6925290d6d186e705b4f887c0ec0ef8b
21797545 <u21797545@tuks.co.za>
2024-08-31 09:58:13 +0200

    🚀Updated YouTube controller

--- Backend/src/youtube/controller/youtube.controller.ts
+++ Backend/src/youtube/controller/youtube.controller.ts
@@ -1,22 +1,42 @@
-import { Body, Controller, Post } from "@nestjs/common";
-import { YoutubeService } from "../services/youtube.service";
+import { Controller, Get, Query, Param, Body, Post, HttpException, HttpStatus } from "@nestjs/common";
+import { YouTubeService } from "../services/youtube.service";
 
-@Controller('youtube')
-export class YoutubeController {
-    constructor(private readonly youtubeService: YoutubeService) {}
-
-    // This endpoint is used to get suggested tracks from the ECHO API (Clustering recommendations).
-    @Post('queue')
-    async getQueue(@Body() body: { artist: string; song_name: string}): Promise<any> {
-        const { artist, song_name } = body;
-        return await this.youtubeService.getQueue(artist, song_name);
+@Controller("youtube")
+export class YouTubeController
+{
+    constructor(private readonly youtubeService: YouTubeService)
+    {
     }
 
-    @Post('track-details')
-    async getTrackDetails(@Body() body: { videoId: string }): Promise<any> {
-        const { videoId } = body;
-        return await this.youtubeService.fetchSingleTrackDetails(videoId);
+    // This endpoint will be used to search for videos/songs on YouTube.
+    @Post("search")
+    async search(@Body() body: { accessToken: string; refreshToken: string; query: string} )
+    {
+        if (!body.accessToken || !body.refreshToken)
+        {
+            throw new HttpException("Access token or refresh token is missing while attempting to search for a song using YouTube.", HttpStatus.UNAUTHORIZED);
+        }
+        if (!body.query)
+        {
+            throw new HttpException("Query is missing while attempting to search for videos on YouTube.", HttpStatus.BAD_REQUEST);
+        }
+        const result = await this.youtubeService.searchVideos(body.query);
+        return result;
     }
 
-
+    // This endpoint will be used to retrieve details of a video/song on YouTube.
+    @Post("video")
+    async getVideo(@Body() body: { accessToken: string; refreshToken: string; id: string})
+    {
+        if (!body.accessToken || !body.refreshToken)
+        {
+            throw new HttpException("Access token or refresh token is missing while attempting to retrieve a song's details on YouTube.", HttpStatus.UNAUTHORIZED);
+        }
+        if (!body.id)
+        {
+            throw new HttpException("Video ID is missing while attempting to retrieve video details from YouTube.", HttpStatus.BAD_REQUEST);
+        }
+        const result = await this.youtubeService.getVideoDetails(body.id);
+        return result;
+    }
 }
