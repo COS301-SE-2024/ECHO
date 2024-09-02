7ca5fc7a7d1ec8cc9dac74be96ba40d10d2ce626
Zion <u21655325@tuks.co.za>
2024-09-02 15:30:43 +0200

    🎉 added profile dropdown

--- Frontend/src/app/components/templates/desktop/other-nav/other-nav.component.ts
+++ Frontend/src/app/components/templates/desktop/other-nav/other-nav.component.ts
@@ -1,15 +1,40 @@
-import { Component } from '@angular/core';
-import {NavbarComponent} from './../../../organisms/navbar/navbar.component';
-import {InfoIconComponent} from './../../../organisms/info-icon/info-icon.component';
-import {ProfileAtomicComponent} from './../../../organisms/profile/profile.component';
-import {MoodDropDownComponent} from './../../../organisms/mood-drop-down/mood-drop-down.component';
+import { Component, HostListener } from '@angular/core';
+import { NavbarComponent } from './../../../organisms/navbar/navbar.component';
+import { InfoIconComponent } from './../../../organisms/info-icon/info-icon.component';
+import { ProfileAtomicComponent } from './../../../organisms/profile/profile.component';
+import { MoodDropDownComponent } from './../../../organisms/mood-drop-down/mood-drop-down.component';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-other-nav',
   standalone: true,
-  imports: [NavbarComponent,InfoIconComponent,ProfileAtomicComponent,MoodDropDownComponent],
+  imports: [
+    NavbarComponent,
+    InfoIconComponent,
+    ProfileAtomicComponent,
+    MoodDropDownComponent,
+    CommonModule
+  ],
   templateUrl: './other-nav.component.html',
   styleUrl: './other-nav.component.css'
 })
-export class OtherNavComponent {
 
+export class OtherNavComponent {
+  isDropdownOpen = false;
+
+  toggleDropdown(): void {
+    console.log('Profile picture clicked. Toggling dropdown...');
+    this.isDropdownOpen = !this.isDropdownOpen;
+    console.log('Dropdown open:', this.isDropdownOpen);
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    const targetElement = event.target as HTMLElement;
+    const dropdownButton = document.getElementById('dropdownInformationButton');
+
+    if (dropdownButton && !dropdownButton.contains(targetElement)) {
+      this.isDropdownOpen = false;
+    }
+  }
 }
