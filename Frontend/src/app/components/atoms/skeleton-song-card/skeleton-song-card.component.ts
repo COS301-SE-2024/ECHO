484c5bf061ac8509a95dc9973a92112cd4e76e61
Rueben  van der Westhuizen <91849806+21434809@users.noreply.github.com>
2024-09-03 22:37:45 +0200

    🚀created skeletone song card

---
+++ Frontend/src/app/components/atoms/skeleton-song-card/skeleton-song-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skeleton-song-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './skeleton-song-card.component.html',
+  styleUrl: './skeleton-song-card.component.css'
+})
+export class SkeletonSongCardComponent {
+
+}
