4dcea1baebf13100035a26c2da2189c426454402
Rueben  van der Westhuizen <91849806+21434809@users.noreply.github.com>
2024-09-10 18:14:29 +0200

    🎉Create expandable icon component for sidebar

--- Frontend/src/app/components/templates/desktop/left/left.component.ts
+++ Frontend/src/app/components/templates/desktop/left/left.component.ts
@@ -1,23 +1,22 @@
-// left.component.ts
 import { Component } from '@angular/core';
 import { NavbarComponent } from './../../../organisms/navbar/navbar.component';
 import { SideBarComponent } from './../../../organisms/side-bar/side-bar.component';
 import { CommonModule } from "@angular/common";
-import { AuthService } from "../../../../services/auth.service";
-import { ProviderService } from "../../../../services/provider.service";
 import { SvgIconComponent } from '../../../atoms/svg-icon/svg-icon.component';
 import { MoodService } from "../../../../services/mood-service.service";
+import { ExpandableIconComponent } from '../../../organisms/expandable-icon/expandable-icon.component';
 
 const SVG_PATHS = {
   PLUS: 'M20 0 H30 V20 H50 V30 H30 V50 H20 V30 H0 V20 H20 Z',
   MIN: 'M0 20 H50 V30 H0 Z',
 };
+
 @Component({
   selector: 'app-left',
   standalone: true,
-  imports: [NavbarComponent, SideBarComponent, CommonModule, SvgIconComponent],
+  imports: [NavbarComponent, SideBarComponent, CommonModule, SvgIconComponent,ExpandableIconComponent],
   templateUrl: './left.component.html',
-  styleUrl: './left.component.css'
+  styleUrls: ['./left.component.css']
 })
 export class LeftComponent {
   constructor(private moodService: MoodService) {}
@@ -29,8 +28,7 @@ export class LeftComponent {
     this.isSideBarHidden = !this.isSideBarHidden;
   }
 
-
   handleSvgClick(event: MouseEvent) {
-    console.log('SVG icon clicked', event);
+      this.svgString = this.svgString === SVG_PATHS.PLUS ? SVG_PATHS.MIN : SVG_PATHS.PLUS;
   }
 }
