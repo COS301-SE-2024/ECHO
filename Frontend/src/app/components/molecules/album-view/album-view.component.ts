c97e9a0027ea964e14d6f9877ddb067259f12ae8
21797545 <u21797545@tuks.co.za>
2024-09-29 22:40:09 +0200

    Revert "Merge pull request #429 from COS301-SE-2024/feat/updateViews"
    This reverts commit 5733a6bcd3dbbdb13137939e77854312f5cb0937, reversing
    changes made to 66e77b7ea5df00bbf9c70ee542de42ea8e6be142.

--- Frontend/src/app/components/molecules/album-view/album-view.component.ts
+++ Frontend/src/app/components/molecules/album-view/album-view.component.ts
@@ -3,32 +3,16 @@ import { MatDialogRef } from "@angular/material/dialog";
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'album-song-view',
+  selector: 'app-album-view',
   standalone: true,
   templateUrl: './album-view.component.html',
-  styleUrls: ['./album-view.component.css'],
+  styleUrl: './album-view.component.css',
   imports: [CommonModule]
 })
-export class SongViewComponent {
-  @Input() selectedAlbum: any = {
-    title: 'After Hours',
-    image: '../assets/images/blinding-lights.png',
-    artist: 'The Weeknd',
-    album: 'After Hours',
-    genre: 'Synthwave/Pop',
-    mood: 'Sad',
-    tracks: [
-      { title: 'Blinding Lights', duration: '3:20' },
-      { title: 'Save Your Tears', duration: '3:35' },
-      { title: 'Heartless', duration: '3:18' },
-      { title: 'In Your Eyes', duration: '3:57' },
-      { title: 'Scared to Live', duration: '3:11' },
-      { title: 'Alone Again', duration: '4:10' },
-      { title: 'Too Late', duration: '3:40' }
-    ]
-  };
+export class AlbumViewComponent {
+  @Input() selectedAlbum: any;
 
-  constructor(public dialogRef: MatDialogRef<SongViewComponent>) {}
+  constructor(public dialogRef: MatDialogRef<AlbumViewComponent>) {}
 
   closeModal() {
     this.dialogRef.close();
