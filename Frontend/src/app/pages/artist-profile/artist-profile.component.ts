6be79073f09dc95672612c78f1203efcf53dbe91
21797545 <u21797545@tuks.co.za>
2024-09-29 23:20:40 +0200

    Revert ":triangular_ruler: Added later commits manually"
    This reverts commit 5c9e2f1384ce6822598222c6a4c92b860e06a9a3.

--- Frontend/src/app/pages/artist-profile/artist-profile.component.ts
+++ Frontend/src/app/pages/artist-profile/artist-profile.component.ts
@@ -16,95 +16,104 @@ import { SpotifyService } from "../../services/spotify.service";
 import { InfoBarComponent } from '../../components/organisms/info-bar/info-bar.component';
 import { MoodService } from '../../services/mood-service.service';
 import { BackButtonComponent } from '../../components/atoms/back-button/back-button.component';
-import { TopCardComponent } from '../../components/molecules/top-card/top-card.component';
 
 @Component({
-  selector: 'app-artist-profile',
-  standalone: true,
-  imports: [
-    NavbarComponent,
-    NgIf,
-    SideBarComponent,
-    MatCard,
-    MatCardContent,
-    MatButtonModule,
-    MatIconModule,
-    NgForOf,
-    EditProfileModalComponent,
-    CommonModule,
-    InfoBarComponent,
-    NgClass,
-    BackButtonComponent,
-    TopCardComponent
-  ],
-  templateUrl: './artist-profile.component.html',
-  styleUrl: './artist-profile.component.css',
+    selector: 'app-artist-profile',
+    standalone: true,
+    imports: [
+        NavbarComponent,
+        NgIf,
+        SideBarComponent,
+        MatCard,
+        MatCardContent,
+        MatButtonModule,
+        MatIconModule,
+        NgForOf,
+        EditProfileModalComponent,
+        CommonModule,
+        InfoBarComponent,
+        NgClass,
+        BackButtonComponent,
+    ],
+    templateUrl: './artist-profile.component.html',
+    styleUrl: './artist-profile.component.css',
 })
-
 export class ArtistProfileComponent implements AfterViewInit {
-  imgpath: string = 'back.jpg';
-  screenSize?: string;
-  currentMood!: string;
-  moodComponentClasses!:{ [key: string]: string };
-  backgroundMoodClasses!:{ [key: string]: string };
+    imgpath: string = 'back.jpg';
+    screenSize?: string;
+    currentMood!: string;
+    moodComponentClasses!:{ [key: string]: string };
+    backgroundMoodClasses!:{ [key: string]: string };
 
-  artist = {
-    name: 'Kendrick Lamar',
-    description: 'Kendrick Lamar is a Grammy-winning rapper and songwriter from Compton, California, known for his thought-provoking lyrics and storytelling. His albums, like Good Kid, M.A.A.D City, To Pimp a Butterfly, and DAMN., explore themes of identity, societal struggles, and personal growth. Widely regarded as one of the most influential voices in hip-hop, Kendricks music blends sharp social commentary with powerful narratives, earning him accolades like the Pulitzer Prize for Music.',
-    genres: ['Hip-Hop', 'Rap', 'Conscious Hip-Hop'],
-    image: './assets/images/kendrick.jpg',
-    topSongs: ['HUMBLE.', 'Swimming Pools (Drank)', 'Money Trees', 'Alright', 'DNA.'],
-    albums: ['Good Kid, M.A.A.D City', 'To Pimp a Butterfly', 'DAMN.', 'Mr. Morale & The Big Steppers'],
-    playlists: ['Kendrick Essentials', 'RapCaviar', 'Hip-Hop Hits', 'Workout Mix'],
-  };
+    artist = {
+        name: 'Kendrick Lamar',
+        description: 'Kendrick Lamar, an influential figure in contemporary music, epitomizes artistic depth and cultural resonance. His discography navigates themes of identity, societal struggle, and personal introspection with poetic precision.',
+        genres: ['Hip-Hop', 'Rap', 'Conscious Hip-Hop'],
+        similarArtists: ['J. Cole', 'Drake', 'Kanye West'],
+        topSongs: ['HUMBLE.', 'Swimming Pools (Drank)', 'Money Trees', 'Alright', 'DNA.'],
+        albums: ['Good Kid, M.A.A.D City', 'To Pimp a Butterfly', 'DAMN.', 'Mr. Morale & The Big Steppers'],
+        features: ['Goosebumps (with Travis Scott)', 'Control (with Big Sean)', 'Collard Greens (with ScHoolboy Q)', 'Pray for Me (with The Weeknd)'],
+        playlists: ['Kendrick Essentials', 'RapCaviar', 'Hip-Hop Hits', 'Workout Mix'],
+      };
 
-  username: string = '';
+    username: string = '';
 
-  constructor(
-    private authService: AuthService,
-    private router: Router,
-    protected dialog: MatDialog,
-    private screenSizeService: ScreenSizeService,
-    private spotifyService: SpotifyService,
-    public moodService: MoodService,
-  ) {
-    this.currentMood = this.moodService.getCurrentMood();
-    this.moodComponentClasses = this.moodService.getComponentMoodClasses();
-  }
-
-  ngAfterViewInit(): void {
-    let currUser = this.authService.currentUser().subscribe((res) => {
-      this.username = res.user.user_metadata.name;
-      this.imgpath = res.user.user_metadata.picture;
-    });
-  }
-
-  async ngOnInit() {
-    this.screenSizeService.screenSize$.subscribe(screenSize => {
-      this.screenSize = screenSize;
-    });
-    if (typeof window !== 'undefined') {
-      await this.spotifyService.init();
+    constructor(
+        private authService: AuthService,
+        private router: Router,
+        protected dialog: MatDialog,
+        private screenSizeService: ScreenSizeService,
+        private spotifyService: SpotifyService,
+        public moodService: MoodService,
+    ) {
+      this.currentMood = this.moodService.getCurrentMood(); 
+      this.moodComponentClasses = this.moodService.getComponentMoodClasses(); 
     }
-  }
 
-  getAlbumArt(item: string): string {
-    // Placeholder function to return a default album art
-    return '../assets/images/damn.jpeg';
-  }
+    ngAfterViewInit(): void {
+      let currUser = this.authService.currentUser().subscribe((res) => {
+        this.username = res.user.user_metadata.name;
+        this.imgpath = res.user.user_metadata.picture;
+      });
+    }
+    async ngOnInit() {
+      this.screenSizeService.screenSize$.subscribe(screenSize => {
+        this.screenSize = screenSize;
+      });
+      if (typeof window !== 'undefined') {
+        await this.spotifyService.init();
+      }
+    }
 
-  playSong(song: string): void {
-    // Logic to play the selected song
-    console.log(`Playing song: ${song}`);
-  }
+    onNavChange($event: string) {}
 
-  viewAlbum(album: string): void {
-    // Logic to view the selected album
-    console.log(`Viewing album: ${album}`);
-  }
+    openDialog(): void {
+        const dialogRef = this.dialog.open(EditProfileModalComponent, {
+            width: '250px',
+        });
 
-  viewPlaylist(playlist: string): void {
-    // Logic to view the selected playlist
-    console.log(`Viewing playlist: ${playlist}`);
-  }
+        dialogRef.afterClosed().subscribe((result) => {
+            console.log('The dialog was closed');
+        });
+
+
+    }
+
+    save() {
+        if (localStorage.getItem('path') !== null) {
+            // @ts-ignore
+            this.imgpath = localStorage.getItem('path');
+        }
+    }
+
+    refresh() {
+      this.authService.currentUser().subscribe((res) => {
+        this.username = res.user.user_metadata.username;
+      });
+    }
+
+    getAlbumArt(song: string): string {
+        // Placeholder function to return a default album art
+        return '../assets/images/damn.jpeg';
+      }
 }
