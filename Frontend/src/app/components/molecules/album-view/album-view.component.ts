7cbd615d5ad7f3a19423a401e3aa6db7958a810b
Zion <u21655325@tuks.co.za>
2024-09-29 17:39:33 +0200

    🚀 improve album view
    Haven't checked yet as its not connected to anything. Need to check after tabbing profile.

--- Frontend/src/app/components/molecules/album-view/album-view.component.ts
+++ Frontend/src/app/components/molecules/album-view/album-view.component.ts
@@ -3,16 +3,32 @@ import { MatDialogRef } from "@angular/material/dialog";
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-album-view',
+  selector: 'album-song-view',
   standalone: true,
   templateUrl: './album-view.component.html',
-  styleUrl: './album-view.component.css',
+  styleUrls: ['./album-view.component.css'],
   imports: [CommonModule]
 })
-export class AlbumViewComponent {
-  @Input() selectedAlbum: any;
+export class SongViewComponent {
+  @Input() selectedAlbum: any = {
+    title: 'After Hours',
+    image: '../assets/images/blinding-lights.png',
+    artist: 'The Weeknd',
+    album: 'After Hours',
+    genre: 'Synthwave/Pop',
+    mood: 'Sad',
+    tracks: [
+      { title: 'Blinding Lights', duration: '3:20' },
+      { title: 'Save Your Tears', duration: '3:35' },
+      { title: 'Heartless', duration: '3:18' },
+      { title: 'In Your Eyes', duration: '3:57' },
+      { title: 'Scared to Live', duration: '3:11' },
+      { title: 'Alone Again', duration: '4:10' },
+      { title: 'Too Late', duration: '3:40' }
+    ]
+  };
 
-  constructor(public dialogRef: MatDialogRef<AlbumViewComponent>) {}
+  constructor(public dialogRef: MatDialogRef<SongViewComponent>) {}
 
   closeModal() {
     this.dialogRef.close();
