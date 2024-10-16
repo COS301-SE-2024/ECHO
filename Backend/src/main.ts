180e7b39b277191509d5c02b2bfb6a4e86bdbfb1
21797545 <u21797545@tuks.co.za>
2024-10-16 20:41:07 +0200

    :triangular_ruler: Updated CORS policy

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -3,21 +3,18 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-    // Create Nest application
     const app = await NestFactory.create(AppModule);
     const configService = app.get(ConfigService);
 
-    // Define allowed origins as an array
     const allowedOrigins = [
         'https://echo-bm8z.onrender.com',
-        'http://localhost:4200'
+        'http://localhost:4200',
+        'https://pwa-test-ocl1.onrender.com'
     ];
 
-    // Enable CORS with dynamic origin checking
     app.enableCors({
         origin: (origin, callback) => {
             if (!origin) return callback(null, true);
-
             if (allowedOrigins.includes(origin)) {
                 callback(null, true);
             } else {
@@ -29,7 +26,7 @@ async function bootstrap() {
     });
 
     app.setGlobalPrefix('api');
-    const port = configService.get<number>('PORT', 3000);
+    const port = process.env.PORT || configService.get<number>('PORT', 3000);
     await app.listen(port);
 
     console.log(`Application is running on: ${await app.getUrl()}`);
