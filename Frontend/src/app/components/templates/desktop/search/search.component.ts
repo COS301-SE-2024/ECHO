0dfaabb0018503c60d79a3bee116e92cdd46cb6d
21797545 <u21797545@tuks.co.za>
2024-10-17 13:21:58 +0200

    :triangular_ruler: Refactored search component

--- Frontend/src/app/components/templates/desktop/search/search.component.ts
+++ Frontend/src/app/components/templates/desktop/search/search.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { NgClass, NgForOf, NgIf, AsyncPipe } from "@angular/common";
-import { SearchService, Track } from "../../../../services/search.service";
+import { AlbumTrack, SearchService, TrackInfo } from "../../../../services/search.service";
 import { Observable } from "rxjs";
 import { ScreenSizeService } from "../../../../services/screen-size-service.service";
 import { Router, ActivatedRoute } from "@angular/router";
@@ -12,24 +12,25 @@ import { SpotifyService } from "../../../../services/spotify.service";
 import { ProviderService } from "../../../../services/provider.service";
 import { YouTubeService } from "../../../../services/youtube.service";
 import { BackButtonComponent } from "../../../atoms/back-button/back-button.component";
+import { SongCardsComponent } from "../../../organisms/song-cards/song-cards.component";
+
 @Component({
   selector: "app-search",
   standalone: true,
-  imports: [NgIf, NgForOf, NgClass, AsyncPipe, TopResultComponent, NavbarComponent, SearchBarComponent,BackButtonComponent],
+  imports: [NgIf, NgForOf, NgClass, AsyncPipe, TopResultComponent, NavbarComponent, SearchBarComponent, BackButtonComponent, SongCardsComponent],
   templateUrl: "./search.component.html",
-  styleUrl: "./search.component.css"
+  styleUrls: ["./search.component.css"]
 })
-export class SearchComponent implements OnInit
-{
+export class SearchComponent implements OnInit {
   // Mood Service Variables
   currentMood!: string;
   moodComponentClasses!: { [key: string]: string };
   backgroundMoodClasses!: { [key: string]: string };
 
   @Input() searchQuery!: string;
-  songs$: Observable<Track[]>;
-  albums$: Observable<Track[]>;
-  topResult$: Observable<Track>;
+  songs$: Observable<TrackInfo[]>;
+  albums$: Observable<AlbumTrack[]>;
+  topResult$: Observable<TrackInfo>;
   screenSize?: string;
   title?: string;
 
@@ -42,8 +43,7 @@ export class SearchComponent implements OnInit
     private spotifyService: SpotifyService,
     private providerService: ProviderService,
     private youtubeService: YouTubeService
-  )
-  {
+  ) {
     this.currentMood = this.moodService.getCurrentMood();
     this.moodComponentClasses = this.moodService.getComponentMoodClasses();
     this.songs$ = this.searchService.getSearch();
@@ -51,71 +51,72 @@ export class SearchComponent implements OnInit
     this.topResult$ = this.searchService.getTopResult();
   }
 
-  async ngOnInit()
-  {
-    this.screenSizeService.screenSize$.subscribe(screenSize =>
-    {
+  async ngOnInit() {
+    this.screenSizeService.screenSize$.subscribe(screenSize => {
       this.screenSize = screenSize;
     });
 
     // Retrieve the query parameter from the URL
-    this.route.queryParams.subscribe(params =>
-    {
+    this.route.queryParams.subscribe(params => {
       this.searchQuery = params["query"] || "";
-      if (this.searchQuery)
-      {
+      if (this.searchQuery) {
         this.performSearch(this.searchQuery);
       }
     });
   }
 
-  onNavChange(newNav: string)
-  {
+  onNavChange(newNav: string) {
     this.title = newNav;
   }
 
-  onSearchdown(search: string)
-  {
+  onSearchdown(search: string) {
     this.searchQuery = search;
   }
 
-  profile()
-  {
+  profile() {
     this.router.navigate(["/profile"]);
   }
 
-  playTrack(name: string, artistName: string)
-  {
-    if (this.providerService.getProviderName() === "spotify")
-    {
-      this.spotifyService.getTrackDetailsByName(name, artistName).then(async (track) =>
-      {
+  playTrack(text: string, secondaryText: string) {
+    if (this.providerService.getProviderName() === "spotify") {
+      this.spotifyService.getTrackDetailsByName(text, secondaryText).then(async (track) => {
         console.log(track);
         await this.spotifyService.playTrackById(track.id);
       });
-    }
-    else
-    {
-      this.youtubeService.getTrackByName(name, artistName).then(async (track) =>
-      {
+    } else {
+      this.youtubeService.getTrackByName(text, secondaryText).then(async (track) => {
         await this.youtubeService.playTrackById(track.id);
       });
     }
   }
 
-  private performSearch(query: string)
-  {
+  private performSearch(query: string) {
     this.searchService.storeSearch(query).subscribe(
-      () =>
-      {
+      () => {
+        // Handle successful search
       },
       error => console.error("Error performing search:", error)
     );
     this.searchService.storeAlbumSearch(query).subscribe(
-      () =>
-      {
+      () => {
+        // Handle successful album search
       },
-      error => console.error("Error performing search:", error)
+      error => console.error("Error performing album search:", error)
     );
   }
+
+  onEchoTrackEvent(event: {
+    trackName: string;
+    artistName: string;
+    event: MouseEvent
+  }, text: string, secondaryText: string)
+  {
+      event.event.stopPropagation();
+      this.router.navigate(['/echo Song'], {
+        queryParams: {
+          trackName: text,
+          artistName: secondaryText
+        }
+      });
+    }
 }
